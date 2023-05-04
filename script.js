474bfa348ee1f0eb74cf5672ebe9292efb8f8734
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2023-05-05 01:31:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,232 +1,233 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-const BLOCK_SIZE = 20;
-const ROWS = 20;
-const COLS = 10;
-const KEY_LEFT = 65; // A
-const KEY_RIGHT = 68; // D
-const KEY_DOWN = 83; // S
-const KEY_ROTATE = 32; // Space
-
-let field = [];
-let block = null;
-let blockX = 0;
-let blockY = 0;
+const boardWidth = 10;
+const boardHeight = 20;
+const board = [];
+let currentBlock;
+let currentBlockType;
+let currentBlockOrientation;
+let currentBlockX;
+let currentBlockY;
 let score = 0;
+let gameInterval;
 
-function init() {
-  // フィールドの初期化
-  field = [];
-  for (let i = 0; i < ROWS; i++) {
-    field[i] = [];
-    for (let j = 0; j < COLS; j++) {
-      field[i][j] = 0;
+function createBoard() {
+  for (let i = 0; i < boardHeight; i++) {
+    board[i] = [];
+    for (let j = 0; j < boardWidth; j++) {
+      board[i][j] = null;
     }
   }
-
-  // スコアの初期化
-  score = 0;
-
-  // ブロックの初期化
-  block = getRandomBlock();
-  blockX = Math.floor(COLS / 2) - Math.floor(block.shape[0].length / 2);
-  blockY = 0;
 }
 
-function getRandomBlock() {
-  const blocks = [
-    {
-      shape: [
-        [1, 1],
-        [1, 1],
-      ],
-      color: "#f00",
-    },
-    {
-      shape: [
-        [0, 2, 0],
-        [2, 2, 2],
-      ],
-      color: "#0f0",
-    },
-    {
-      shape: [
-        [0, 3, 3],
-        [3, 3, 0],
-      ],
-      color: "#00f",
-    },
-    {
-      shape: [
-        [4, 4, 0],
+function createBlock() {
+  const blockTypes = ['i', 'o', 't', 's', 'z', 'j', 'l'];
+  const blockOrientations = [0, 90, 180, 270];
+  const randomType = blockTypes[Math.floor(Math.random() * blockTypes.length)];
+  const randomOrientation = blockOrientations[Math.floor(Math.random() * blockOrientations.length)];
+  currentBlockType = randomType;
+  currentBlockOrientation = randomOrientation;
+  currentBlockX = Math.floor(boardWidth / 2) - 1;
+  currentBlockY = 0;
+  switch (currentBlockType) {
+    case 'i':
+      currentBlock = [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ];
+      break;
+    case 'o':
+      currentBlock = [
+        [2, 2],
+        [2, 2]
+      ];
+      break;
+    case 't':
+      currentBlock = [
+        [0, 3, 0],
+        [3, 3, 3],
+        [0, 0, 0]
+      ];
+      break;
+    case 's':
+      currentBlock = [
         [0, 4, 4],
-      ],
-      color: "#ff0",
-    },
-    {
-      shape: [
-        [0, 5, 0],
-        [5, 5, 5],
-      ],
-      color: "#f0f",
-    },
-    {
-      shape: [
-        [6, 6, 6, 6],
-      ],
-      color: "#0ff",
-    },
-    {
-      shape: [
-        [7, 7],
-        [7, 7],
-      ],
-      color: "#f80",
-    },
-  ];
-
-  const index = Math.floor(Math.random() * blocks.length);
-  return blocks[index];
+        [4, 4, 0],
+        [0, 0, 0]
+      ];
+      break;
+    case 'z':
+      currentBlock = [
+        [5, 5, 0],
+        [0, 5, 5],
+        [0, 0, 0]
+      ];
+      break;
+    case 'j':
+      currentBlock = [
+        [6, 0, 0],
+        [6, 6, 6],
+        [0, 0, 0]
+      ];
+      break;
+    case 'l':
+      currentBlock = [
+        [0, 0, 7],
+        [7, 7, 7],
+        [0, 0, 0]
+      ];
+      break;
+  }
 }
 
-function drawBlock(x, y, block) {
-  ctx.fillStyle = block.color;
-  for (let i = 0; i < block.shape.length; i++) {
-    for (let j = 0; j < block.shape[i].length; j++) {
-      if (block.shape[i][j] !== 0) {
-        ctx.fillRect(
-          (j + x) * BLOCK_SIZE,
-          (i + y) * BLOCK_SIZE,
-          BLOCK_SIZE,
-          BLOCK_SIZE
-        );
+function drawBlock() {
+  for (let i = 0; i < currentBlock.length; i++) {
+    for (let j = 0; j < currentBlock[i].length; j++) {
+      if (currentBlock[i][j] !== 0) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.classList.add(`block-${currentBlockType}`);
+        block.style.left = `${(currentBlockX + j) * 30}px`;
+        block.style.top = `${(currentBlockY + i) * 30}px`;
+        document.getElementById('game-board').appendChild(block);
       }
     }
   }
 }
 
-function drawField() {
-  for (let i = 0; i < ROWS; i++) {
-    for (let j = 0; j < COLS; j++) {
-      if (field[i][j] !== 0) {
-        ctx.fillStyle = field[i][j];
-        ctx.fillRect(
-          j * BLOCK_SIZE,
-          i * BLOCK_SIZE,
-          BLOCK_SIZE,
-          BLOCK_SIZE
-        );
-      }
-    }
+function clearBoard() {
+  const blocks = document.getElementsByClassName('block');
+  while (blocks.length > 0) {
+    blocks[0].parentNode.removeChild(blocks[0]);
   }
 }
 
-function isCollision(x, y, block) {
-  for (let i = 0; i < block.shape.length; i++) {
-    for (let j = 0; j < block.shape[i].length; j++) {
-      if (
-        block.shape[i][j] !== 0 &&
-        (y + i >= ROWS ||
-          x + j < 0 ||
-          x + j >= COLS ||
-          field[y + i][x + j] !== 0)
-      ) {
-        return true;
+function checkCollision(x, y, block) {
+  for (let i = 0; i < block.length; i++) {
+    for (let j = 0; j < block[i].length; j++) {
+      if (block[i][j] !== 0) {
+        const boardX = x + j;
+        const boardY = y + i;
+        if (boardX < 0 || boardX >= boardWidth || boardY >= boardHeight || (boardY >= 0 && board[boardY][boardX] !== null)) {
+          return true;
+        }
       }
     }
   }
   return false;
 }
 
-function addBlockToField(x, y, block) {
-  for (let i = 0; i < block.shape.length; i++) {
-    for (let j = 0; j < block.shape[i].length; j++) {
-      if (block.shape[i][j] !== 0) {
-        field[y + i][x + j] = block.color;
-      }
+function rotateBlock() {
+  const newBlock = [];
+  for (let i = 0; i < currentBlock[0].length; i++) {
+    newBlock[i] = [];
+    for (let j = 0; j < currentBlock.length; j++) {
+      newBlock[i][j] = currentBlock[currentBlock.length - 1 - j][i];
     }
+  }
+  if (!checkCollision(currentBlockX, currentBlockY, newBlock)) {
+    currentBlock = newBlock;
+    currentBlockOrientation = (currentBlockOrientation + 90) % 360;
+    clearBoard();
+    drawBlock();
   }
 }
 
-function removeLine() {
-  let numRemovedLines = 0;
-  for (let i = ROWS - 1; i >= 0; i--) {
-    if (field[i].every((cell) => cell !== 0)) {
-      field.splice(i, 1);
-      field.unshift(new Array(COLS).fill(0));
-      numRemovedLines++;
-    }
+function moveBlockLeft() {
+  if (!checkCollision(currentBlockX - 1, currentBlockY, currentBlock)) {
+    currentBlockX--;
+    clearBoard();
+    drawBlock();
   }
-  return numRemovedLines;
 }
 
-function update() {
-  // ブロックを下に移動
-  if (!isCollision(blockX, blockY + 1, block)) {
-    blockY++;
+function moveBlockRight() {
+  if (!checkCollision(currentBlockX + 1, currentBlockY, currentBlock)) {
+    currentBlockX++;
+    clearBoard();
+    drawBlock();
+  }
+}
+
+function moveBlockDown() {
+  if (!checkCollision(currentBlockX, currentBlockY + 1, currentBlock)) {
+    currentBlockY++;
+    clearBoard();
+    drawBlock();
   } else {
-    addBlockToField(blockX, blockY, block);
-    const numRemovedLines = removeLine();
-    if (numRemovedLines > 0) {
-      score += 100 * numRemovedLines;
-    }
-    block = getRandomBlock();
-    blockX = Math.floor(COLS / 2) - Math.floor(block.shape[0].length / 2);
-    blockY = 0;
-    if (isCollision(blockX, blockY, block)) {
-      alert("Game Over!");
-      init();
+    addBlockToBoard();
+    checkRows();
+    createBlock();
+    clearBoard();
+    drawBoard();
+    drawBlock();
+  }
+}
+
+function addBlockToBoard() {
+  for (let i = 0; i < currentBlock.length; i++) {
+    for (let j = 0; j < currentBlock[i].length; j++) {
+      if (currentBlock[i][j] !== 0) {
+        board[currentBlockY + i][currentBlockX + j] = currentBlockType;
+      }
     }
   }
 }
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawField();
-  drawBlock(blockX, blockY, block);
+function checkRows() {
+  let rowsCleared = 0;
+  for (let i = board.length - 1; i >= 0; i--) {
+    if (board[i].every(block => block !== null)) {
+      board.splice(i, 1);
+      board.unshift(new Array(boardWidth).fill(null));
+      rowsCleared++;
+      i++;
+    }
+  }
+  if (rowsCleared > 0) {
+    score += rowsCleared * 100;
+    document.getElementById('score').innerHTML = `Score: ${score}`;
+  }
 }
 
-function loop() {
-  update();
-  draw();
+function drawBoard() {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (board[i][j] !== null) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.classList.add(`block-${board[i][j]}`);
+        block.style.left = `${j * 30}px`;
+        block.style.top = `${i * 30}px`;
+        document.getElementById('game-board').appendChild(block);
+      }
+    }
+  }
 }
 
-init();
-setInterval(loop, 1000 / 60);
+function startGame() {
+  createBoard();
+  createBlock();
+  drawBoard();
+  drawBlock();
+  gameInterval = setInterval(moveBlockDown, 500);
+}
 
-document.addEventListener("keydown", (event) => {
+document.addEventListener('keydown', event => {
   switch (event.keyCode) {
-    case KEY_LEFT:
-      if (!isCollision(blockX - 1, blockY, block)) {
-        blockX--;
-      }
+    case 65: // A
+      moveBlockLeft();
       break;
-    case KEY_RIGHT:
-      if (!isCollision(blockX + 1, blockY, block)) {
-        blockX++;
-      }
+    case 68: // D
+      moveBlockRight();
       break;
-    case KEY_DOWN:
-      if (!isCollision(blockX, blockY + 1, block)) {
-        blockY++;
-      }
+    case 83: // S
+      moveBlockDown();
       break;
-    case KEY_ROTATE:
-      const rotatedBlock = {
-        shape: [],
-        color: block.color,
-      };
-      for (let i = 0; i < block.shape[0].length; i++) {
-        const newRow = [];
-        for (let j = block.shape.length - 1; j >= 0; j--) {
-          newRow.push(block.shape[j][i]);
-        }
-        rotatedBlock.shape.push(newRow);
-      }
-      if (!isCollision(blockX, blockY, rotatedBlock)) {
-        block = rotatedBlock;
-      }
+    case 87: // W
+      rotateBlock();
       break;
   }
 });
+
+startGame();
